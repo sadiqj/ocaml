9e5418f0c24789fe1c2a75724911ef4e4248dd38
Sadiq Jaffer <sadiq@toao.com>
2020-09-08 04:34:03 +0100

    make 16

--- runtime/major_gc.c
+++ runtime/major_gc.c
@@ -234,7 +234,7 @@ Caml_inline void mark_stack_push(struct mark_stack* stk, value block,
   }
 #endif
 
-  end = (block_wsz < 8 ? block_wsz : 8);
+  end = (block_wsz < 16 ? block_wsz : 16);
 
   /* Optimisation to avoid pushing small, unmarkable objects such as [Some 42]
    * into the mark stack. */

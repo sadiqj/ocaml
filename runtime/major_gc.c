4ed156b4f6c637ae53d36c5a38dda5a1a240b60b
Sadiq Jaffer <sadiq@toao.com>
2020-09-08 04:34:48 +0100

    make 64

--- runtime/major_gc.c
+++ runtime/major_gc.c
@@ -234,7 +234,7 @@ Caml_inline void mark_stack_push(struct mark_stack* stk, value block,
   }
 #endif
 
-  end = (block_wsz < 32 ? block_wsz : 32);
+  end = (block_wsz < 64 ? block_wsz : 64);
 
   /* Optimisation to avoid pushing small, unmarkable objects such as [Some 42]
    * into the mark stack. */

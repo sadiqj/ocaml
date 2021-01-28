45a62a74e22b5284a97c5e16c0a4332859641899
Sadiq Jaffer <sadiq@toao.com>
2021-01-28 14:48:12 +0000

    force a major slice whenever we request a minor gc in this test

--- testsuite/tests/asmcomp/polling.c
+++ testsuite/tests/asmcomp/polling.c
@@ -6,6 +6,7 @@
 
 CAMLprim value request_minor_gc(value v) {
   Caml_state->requested_minor_gc = 1;
+  Caml_state->requested_major_slice = 1;
   caml_something_to_do = 1;
   Caml_state->young_limit = Caml_state->young_alloc_end;
 }

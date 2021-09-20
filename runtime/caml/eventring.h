20a6d0863f9c7bb1f0b94eee5dbdd65eb8b5500a
Sadiq Jaffer <sadiq@toao.com>
2021-09-20 15:27:31 +0100

    flesh out ocaml api

--- runtime/caml/eventring.h
+++ runtime/caml/eventring.h
@@ -16,6 +16,7 @@
 #define CAML_EVENTRING_H
 
 #include "eventlog.h"
+#include "mlvalues.h"
 #include <stdint.h>
 
 #define CAML_EVENTRING_INIT() caml_eventring_init()
@@ -26,7 +27,7 @@
 #define CAML_EV_ALLOC(s) caml_ev_alloc(s)
 #define CAML_EV_ALLOC_FLUSH() caml_ev_alloc_flush()
 #else
-#define CAML_EV_ALLOC(s) /**/
+#define CAML_EV_ALLOC(s)      /**/
 #define CAML_EV_ALLOC_FLUSH() /**/
 #endif
 
@@ -36,12 +37,16 @@
 struct caml_eventring_cursor;
 
 struct caml_eventring_callbacks {
-    void (*ev_runtime_begin)(void* callback_data, uint64_t timestamp, ev_runtime_phase phase);
-    void (*ev_runtime_end)(void* callback_data, uint64_t timestamp, ev_runtime_phase phase);
-    void (*ev_runtime_counter)(void* callback_data, uint64_t timestamp, ev_runtime_counter counter, uint64_t val);
-    void (*ev_alloc)(void* callback_data, uint64_t timestamp, uint64_t* sz);
-    void (*ev_lifecycle)(void* callback_data, int64_t timestamp, ev_lifecycle lifecycle);
-    void (*ev_lost_events)(void* callback_data, int lost_events);
+  void (*ev_runtime_begin)(void *callback_data, uint64_t timestamp,
+                           ev_runtime_phase phase);
+  void (*ev_runtime_end)(void *callback_data, uint64_t timestamp,
+                         ev_runtime_phase phase);
+  void (*ev_runtime_counter)(void *callback_data, uint64_t timestamp,
+                             ev_runtime_counter counter, uint64_t val);
+  void (*ev_alloc)(void *callback_data, uint64_t timestamp, uint64_t *sz);
+  void (*ev_lifecycle)(void *callback_data, int64_t timestamp,
+                       ev_lifecycle lifecycle);
+  void (*ev_lost_events)(void *callback_data, int lost_events);
 };
 
 /* Starts eventring. Needs to be called before [caml_eventring_create_cursor] */
@@ -51,29 +56,29 @@ extern value caml_eventring_start();
     process id (or equivalent) of the startup OCaml process. This function will
     return a cursor which can we be used with caml_eventring_read_poll to read
     events from the eventrings. */
-extern struct caml_eventring_cursor* caml_eventring_create_cursor(
-                                        char* eventring_path,
-                                        int pid);
+extern struct caml_eventring_cursor* caml_eventring_create_cursor(const char *eventring_path, int pid);
 
 /* frees a cursor obtained from caml_eventring_creator_cursor */
-extern void caml_eventring_free_cursor(struct caml_eventring_cursor* cursor);
+extern void caml_eventring_free_cursor(struct caml_eventring_cursor *cursor);
 
 /* polls the eventring pointed to by [cursor] and calls the appropriate callback
     provided in [callbacks] for each new event. Returns the number of events
     consumed. [callback_data] is an optional pointer that can be passed to
     each callback. */
-CAMLextern int caml_eventring_read_poll(struct caml_eventring_cursor* cursor,
-                             struct caml_eventring_callbacks* callbacks,
-                             void* callback_data);
+CAMLextern int caml_eventring_read_poll(struct caml_eventring_cursor *cursor,
+                         struct caml_eventring_callbacks *callbacks,
+                         void *callback_data);
 
 /* OCaml API for reading from the eventring */
-extern value caml_eventring_create_wrapped_cursor(value eventring_path, value pid);
+extern value caml_eventring_create_wrapped_cursor(value eventring_path,
+                                                  value pid);
 extern value caml_eventring_free_wrapped_cursor(value wrapped_cursor);
-extern value caml_eventring_read_poll_wrapped(value wrapped_cursor, value callbacks);
+extern value caml_eventring_read_poll_wrapped(value wrapped_cursor,
+                                              value callbacks);
 
 #ifdef CAML_INTERNALS
 
- /* Functions for putting runtime data on to the eventring */
+/* Functions for putting runtime data on to the eventring */
 
 void caml_eventring_init();
 void caml_eventring_destroy();
@@ -89,4 +94,3 @@ void caml_ev_flush();
 #endif /* CAML_INTERNALS */
 
 #endif /*CAML_EVENTRING_H*/
-

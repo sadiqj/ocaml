f16826e91106ed581f3a5a8707ddac9356a1a453
Sadiq Jaffer <sadiq@toao.com>
2022-01-11 18:06:40 +0000

    promote tests

--- testsuite/tests/shapes/rotor_example.ml
+++ testsuite/tests/shapes/rotor_example.ml
@@ -26,8 +26,8 @@ end
 {
  ("Pair", module) ->
      Abs<.9>
-        (X/284,
-         Abs(Y/285, {
+        (X/285,
+         Abs(Y/286, {
                      ("t", type) -> <.5>;
                      ("to_string", value) -> <.6>;
                      }));

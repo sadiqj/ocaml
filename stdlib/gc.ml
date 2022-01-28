ocaml-multicore/ocaml-multicore

event ringbuffer (eventring)

stdlib/gc.ml
@@ -64,8 +64,8 @@ external compact : unit -> unit = "caml_gc_compaction"
 (* external get_bucket : int -> int = "caml_get_major_bucket" [@@noalloc] *)
 (* external get_credit : unit -> int = "caml_get_major_credit" [@@noalloc] *)
 (* external huge_fallback_count : unit -> int = "caml_gc_huge_fallback_count" *)
-external eventlog_pause : unit -> unit = "caml_eventlog_pause"
-external eventlog_resume : unit -> unit = "caml_eventlog_resume"
+external eventlog_pause : unit -> unit = "caml_eventring_pause"
+external eventlog_resume : unit -> unit = "caml_eventring_resume"
 
 open Printf
 

sadiqj/ocaml

stdlib/gc.ml
@@ -61,8 +61,8 @@ external major : unit -> unit = "caml_gc_major"
 external full_major : unit -> unit = "caml_gc_full_major"
 external compact : unit -> unit = "caml_gc_compaction"
 external get_minor_free : unit -> int = "caml_get_minor_free"
-external eventlog_pause : unit -> unit = "caml_eventlog_pause"
-external eventlog_resume : unit -> unit = "caml_eventlog_resume"
+external eventring_pause : unit -> unit = "caml_eventring_pause"
+external eventring_resume : unit -> unit = "caml_eventring_resume"
 
 open Printf
 
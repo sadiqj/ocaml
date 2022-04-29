7eea33d3e5f4e253c30f3bd9e2051a3eb1452b0a
Sadiq Jaffer <sadiq@toao.com>
2022-04-29 15:18:10 +0100

    split out runtime_events and runtime_events_consumer headers so things work on windows

--- runtime/caml/runtime_events.h
+++ runtime/caml/runtime_events.h
@@ -172,70 +172,6 @@ extern value caml_runtime_events_pause();
    written to this domain immediately and all other domains soon. */
 extern value caml_runtime_events_resume();
 
-/* Create a cursor to read events from an runtime_events. Cursors can be created
-   for runtime_eventss in and out of process. An runtime_events may have
-   multiple cursors reading from it at any point in time and a program may have
-   multiple cursors open concurrently (for example if multiple consumers want
-   different sets of events). To create one for the current process, pass
-   [runtime_events_path] as NULL and a [pid] < 0. Otherwise
-   [runtime_events_path] is a path to a directory containing the .runtime_events
-   files. [pid] is the process id (or equivalent) of the startup OCaml process.
-   The resulting cursor can be used with `caml_runtime_events_read_poll` to read
-   events from the runtime_events ring-buffers. */
-extern runtime_events_error
-caml_runtime_events_create_cursor(const char_os* runtime_events_path, int pid,
-                             struct caml_runtime_events_cursor **cursor_res);
-
-/* Set the runtime_begin event callback on the cursor */
-extern void caml_runtime_events_set_runtime_begin(
-    struct caml_runtime_events_cursor *cursor,
-    int (*f)(int domain_id, void *callback_data, uint64_t timestamp,
-             ev_runtime_phase phase));
-
-/* Set the runtime_end event callback on the cursor */
-extern void caml_runtime_events_set_runtime_end(
-    struct caml_runtime_events_cursor *cursor,
-    int (*f)(int domain_id, void *callback_data, uint64_t timestamp,
-             ev_runtime_phase phase));
-
-/* Set the runtime_counter event callback on the cursor */
-extern void caml_runtime_events_set_runtime_counter(
-    struct caml_runtime_events_cursor *cursor,
-    int (*f)(int domain_id, void *callback_data, uint64_t timestamp,
-             ev_runtime_counter counter, uint64_t val));
-
-/* Set the alloc event callback on the cursor */
-extern void
-caml_runtime_events_set_runtime_alloc(struct caml_runtime_events_cursor *cursor,
-                                 int (*f)(int domain_id, void *callback_data,
-                                          uint64_t timestamp, uint64_t *sz));
-
-/* Set the lifecycle event callback on the cursor */
-extern void caml_runtime_events_set_lifecycle(
-    struct caml_runtime_events_cursor *cursor,
-    int (*f)(int domain_id, void *callback_data, int64_t timestamp,
-              ev_lifecycle lifecycle, int64_t data));
-
-/* Set the lost events callback on the cursor */
-extern void caml_runtime_events_set_lost_events(
-    struct caml_runtime_events_cursor *cursor,
-    int (*f)(int domain_id, void *callback_data, int lost_words));
-
-/* frees a cursor obtained from caml_runtime_events_creator_cursor */
-extern void
-caml_runtime_events_free_cursor(struct caml_runtime_events_cursor *cursor);
-
-/* polls the runtime_events pointed to by [cursor] and calls the appropriate
-   callback for each new event up to at most [max_events] times.
-
-    Returns the number of events consumed in [events_consumed], if set.
-
-    0 for [max_events] indicates no limit to the number of callbacks. */
-CAMLextern runtime_events_error caml_runtime_events_read_poll(
-    struct caml_runtime_events_cursor *cursor,
-    void *callback_data,
-    uintnat max_events, uintnat *events_consumed);
-
 #ifdef CAML_INTERNALS
 
 struct runtime_events_buffer_header {
@@ -288,7 +224,7 @@ void caml_runtime_events_post_fork();
 
 /* Returns the location of the runtime_events for the current process if started
    or NULL otherwise */
-char_os* caml_runtime_events_current_location();
+CAMLextern char_os* caml_runtime_events_current_location();
 
 /* Functions for putting runtime data on to the runtime_events */
 void caml_ev_begin(ev_runtime_phase phase);
